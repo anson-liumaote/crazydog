37a19136c54b6a29eb92e6af86a8ad5d576a29d0
Anson <maote.liu@gmail.com>
2024-12-23 18:11:09 +0800

    update launch file

--- crazydog_ws/src/robot_interfaces/launch/robotv1_interface.launch.py
+++ crazydog_ws/src/robot_interfaces/launch/robotv1_interface.launch.py
@@ -38,12 +38,6 @@ def generate_launch_description():
             executable='joy_controller',
             name='joy_controller'
         ),
-        # Node(
-        #     package='robot_interfaces',
-        #     executable='unitree_pubsub',
-        #     name='unitree_pubsub',
-        #     parameters=[config]
-        # ),
         Node(
             package='robot_interfaces',
             executable='unitree_left_pub',
